78311c56536f13f409249a69531ca7373a629b6c
erisson <erissonmtg85@gmail.com>
2026-06-14 21:32:59 -0300

    atividade2

--- js/script_questao02.js
+++ js/script_questao02.js
@@ -5,6 +5,6 @@ document.getElementById("formulario-pessoa").addEventListener("submit", function
     const altura = parseFloat(document.getElementById("altura").value);
 
     const area = largura * altura;
-    const tinta = Math.ceil(area / 3); // cada litro pinta 2 metros quadrados
+    const tinta = Math.ceil(area / 2); // cada litro pinta 2 metros quadrados
     document.getElementById("div-dados").innerHTML = `<p>Área da parede: <strong>${area} m²</strong><br>Quantidade de tinta necessária: <strong>${tinta} litros</strong></p>`;
 });
